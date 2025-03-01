278cfd51767162932e35840b72db9f71171c3a01
Aleksandar Nyagolov <aleksandar_niagolov@abv.bg>
2025-03-01 09:55:44 +0200

    projects add

--- views/home.php
+++ views/home.php
@@ -9,10 +9,15 @@
 </head>
 <body>
     <header>
-        <h1 id="name">John Doe</h1>
+        <h1 id="name">Aleksandar Nyagolov</h1>
         <p id="bio">Full-Stack Developer | Cybersecurity Enthusiast</p>
         <button onclick="toggleTheme()">Toggle Theme</button>
     </header>
+
+    <section class="summary" id="summary">
+        <img src="" alt="">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, laborum eaque quidem deleniti repellendus autem aspernatur architecto beatae officiis fugiat possimus neque quibusdam, impedit provident perspiciatis in quas non! Ut.</p>
+    </section>
     
     <section id="skills">
         <h2>Skills</h2>
@@ -21,12 +26,26 @@
             <li>PHP</li>
             <li>React</li>
             <li>MySQL</li>
+            <li>HTML&CSS</li>
         </ul>
     </section>
 
-    <section id="experience">
-        <h2>Experience</h2>
-        <p>Software Engineer at XYZ Corp</p>
+    <section id="projects">
+    <h3>Hotel Website Project</h3>
+    <p>Summer villa website, that features creating online reservations and and commenting on
+    rooms and user authentication</p>
+    <h4>Technologies: </h4><span>React, Node.js
+    </span>
+
+    <h3>CookBook</h3>
+    <p>Web-application that allows the user to view, create, update, delete, recommend and serach
+    recipes. User authentication implemented</p>
+    <h4>Technologies: </h4><span>Node.js, Express, MongoDB, JWT, RESTful APIs</span>
+
+    <h3>Recipes Book</h3>
+    <p>Application that allows you to see,, upload and comment cooking ecipes</p>
+    <h4>Technology:</h4><span>Angular, Node.js</span>
+ 
     </section>
     
     <section id="contact">
