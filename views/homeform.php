a2591d6ed60db816d2e430c90df45e83ffc1ab2e
bozh-anka <81514272+bozh-anka@users.noreply.github.com>
2025-03-01 12:47:07 +0200

    Update homeform.php

--- views/homeform.php
+++ views/homeform.php
@@ -3,52 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Interactive Web CV Form</title>
+    <title>Interactive Web CV</title>
     <link rel="stylesheet" href="../assests/css/homeform.css">
     <script defer src="../assests//js/home.js"></script>
 </head>
 <body>
    
 	<body>
-	<h3>Click on the fields to fill in your information.</h3>
-	<form class="main" form method="POST" action="">
-	 <h1 id="name"> 
-		<input type="text" id="nameInput" value="Full Name"></h1>
-        <input id="keyword" value="Insert keywords."> 
+	<div class="main" >
+	 <h1 id="name"> John Doe </h1>
+        <p id="keyword">Skill| Passion | Hobby</p> 
     <section class="summary" id="summary">
-        <input type="image" id="image" src="https://www.exscribe.com/wp-content/uploads/2021/08/placeholder-image-person-jpg.jpg" alt="Photograph of the user" width = "150" height="150">
-        <textarea id="bio" rows='7'>Enter your biography/summary.</textarea>
+        <img id="image" src="https://www.exscribe.com/wp-content/uploads/2021/08/placeholder-image-person-jpg.jpg" alt="Photograph of the user" width = "150" height="150">
+        <p id="bio" rows='7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Morbi eget tempus turpis. Suspendisse eu ligula orci. Quisque sagittis posuere purus, vel rhoncus mi feugiat sit amet. 
+		Mauris posuere dapibus sapien, ut varius nibh mattis vitae. 
+		Pellentesque vel risus lectus. Nullam mattis nibh quam, et finibus massa ornare in. 
+		Nunc consequat tincidunt sapien posuere euismod. Nam commodo, metus eget pharetra ullamcorper, mi ex volutpat turpis, at commodo dui libero quis arcu.
+		Mauris eu tortor egestas, luctus turpis sit amet, commodo arcu. Maecenas pretium consectetur sapien id ultrices. 
+		Suspendisse potenti. Praesent non massa ac mi malesuada pulvinar. Nam a orci eget libero malesuada rhoncus vitae nec ante. 
+		Integer aliquet ipsum quis consectetur fringilla. Cras convallis est in dolor molestie, nec porttitor sem egestas.</p>
     </section>
     
     <section id="skills">
         <h2>Skills</h2>
-		<input type='text' id='skill' value="Add Skill"/>
-		<!- This input needs to process into a list of skills that both displays below and is added to the json->
-
 		<ul id='list'>
-            
+            <li> Skill 1</li>
+			<li> Skill 2</li>
+			<li> Skill 3</li>
+			<li> Skill 4</li>
         </ul>
     </section>
 
     <section id="projects">
-	<h2>Projects</h2>
-	<h3><input id="project" type="text" value="Project Title"></h3>
-	<p><input id="projectDesc" type="text" value="Project description-"></p>
-	<h4><input id="tech" type="text" value="Tech stack-"></h4>
-
+	<h2>Projects</h2><ul>
+	<li><h3>Passion project 1</h3>
+	<p>Ut ut elit vel turpis sollicitudin bibendum. Nam fermentum diam quis hendrerit fermentum. Mauris maximus dignissim lorem sit amet pulvinar. Vivamus in viverra lacus, a dictum orci.
+	Praesent eu tellus et dolor facilisis rutrum. Pellentesque fringilla dapibus sagittis. 
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+	<h4>Tech 1 | Tech 2 | Tech 3</h4>
+	</li>
+	<li>
+	<h3>Passion project 2</h3>
+	<p>Ut ut elit vel turpis sollicitudin bibendum. Nam fermentum diam quis hendrerit fermentum. Mauris maximus dignissim lorem sit amet pulvinar. Vivamus in viverra lacus, a dictum orci.
+	Praesent eu tellus et dolor facilisis rutrum. Pellentesque fringilla dapibus sagittis. 
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+	<h4>Tech 1 | Tech 2 | Tech 3</h4>
+	</li>
+	</ul>
     </section>
     <section id="experience">
-	<h2>Experience</h2>
-	<h3><input id="exp" type="text" value="Work experience"></h3>
-
-
+	<h2>Experience</h2><ul>
+	<li><h3> Software engineer at XZY</h3></li>
+	<li><h3> Front end at ABC</h3></li>
+	</ul>
     </section>
-    
 	
-	<button type="submit">
-                    Create CV
-    </button>
-	</form>
+	 <section id="contact">
+        <h2>Contact</h2>
+        <form action="contact.php" method="POST">
+            <input type="text" name="name" placeholder="Your Name" required>
+            <input type="email" name="email" placeholder="Your Email" required>
+            <textarea name="message" placeholder="Your Message"></textarea>
+            <button type="submit">Send</button>
+        </form>
+   </div>
     </body>
 </body>
 </html>
