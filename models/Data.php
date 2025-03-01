4498ffbb2469473c16243290921327ec06338c57
Aleksandar Nyagolov <aleksandar_niagolov@abv.bg>
2025-03-01 11:04:54 +0200

    test

---
+++ models/Data.php
@@ -0,0 +1,20 @@
+<?php
+
+class Data{
+    private $conn;
+
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+
+    public function getData(){
+        
+    }
+}
+
+
+
+?>
