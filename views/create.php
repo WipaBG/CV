7ca86302be7c6193d3942f8e1753780b2258a37b
bozh-anka <81514272+bozh-anka@users.noreply.github.com>
2025-03-01 15:03:40 +0200

    Form fixing

--- views/create.php
+++ views/create.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Interactive Web CV Form</title>
-    <link rel="stylesheet" href="../assests/css/homeform.css">
+    <link rel="stylesheet" href="../assests/css/create.css">
     <script defer src="../assests//js/home.js"></script>
 </head>
 <body>
@@ -13,16 +13,18 @@
 	<h3>Click on the fields to fill in your information.</h3>
 	<form class="main" action="">
 	 <h1 id="name"> 
-		<input type="text" id="nameInput" value="Full Name"></h1>
-        <input id="keyword" value="Insert keywords."> 
-    <section class="summary" id="summary">
-        <input type="text" id="image">
-        <textarea id="bio" rows='7'>Enter your biography/summary.</textarea>
+		<input type="text" id="nameInput" value="Full Name" name="name"></h1>
+	<section class="image" id="image" name="image">
+	
+	<input type="text" id="image" value="Insert url for CV photo">
+	</section>
+    <section class="education" id="education" name="education">
+        <textarea id="bio" rows='7'>Enter your education.</textarea>
     </section>
     
     <section id="skills">
         <h2>Skills</h2>
-		<input type='text' id='skill' value="Add Skill"/>
+		<input type='text' id='skill' value="Add Skill" name="skill"/>
 		<!- This input needs to process into a list of skills that both displays below and is added to the json->
 
 		<ul id='list'>
@@ -32,9 +34,7 @@
 
     <section id="projects">
 	<h2>Projects</h2>
-	<h3><input id="project" type="text" value="Project Title"></h3>
-	<p><input id="projectDesc" type="text" value="Project description-"></p>
-	<h4><input id="tech" type="text" value="Tech stack-"></h4>
+	<h3><input id="project" type="text" value="Insert project information" name="project"></h3>
 
     </section>
     
