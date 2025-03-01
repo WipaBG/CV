042e2580c12e26eee9bfe516375e72e149a4432f
bozh-anka <81514272+bozh-anka@users.noreply.github.com>
2025-03-01 15:17:09 +0200

    name fix

--- views/create.php
+++ views/create.php
@@ -11,14 +11,14 @@
 
 <body>
    
-	<body>
+	
 	<h3>Click on the fields to fill in your information.</h3>
 	<form class="main" action="index.php?action=createCV" method= "POST">
 	 <h1 id="name"> 
 		<input type="text" id="nameInput" value="Full Name" name="name"></h1>
-	<section class="image" id="image" name="image">
+	<section class="image" id="image" >
 	
-	<input type="text" id="image" value="Insert url for CV photo">
+	<input type="text" id="image"  name="image" value="Insert url for CV photo">
 	</section>
     <section class="education" id="education">
         <textarea id="bio" rows='7' name="education">Enter your education.</textarea>
@@ -27,7 +27,7 @@
     <section id="skills">
         <h2>Skills</h2>
 		<input type='text' id='skill' value="Add Skill" name="skill"/>
-		<!- This input needs to process into a list of skills that both displays below and is added to the json->
+		
 
                     <ul id='list'>
 
@@ -45,6 +45,6 @@
                 Create CV
             </button>
         </form>
-    </body>
+   
 </body>
 </html>
