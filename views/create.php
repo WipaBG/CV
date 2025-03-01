1d935e4f0fbbbf9ab2e4dc624a57abe0dfd7e246
Aleksandar Nyagolov <aleksandar_niagolov@abv.bg>
2025-03-01 15:29:16 +0200

    create controller

--- views/create.php
+++ views/create.php
@@ -10,41 +10,44 @@
 </head>
 
 <body>
-   
-	
-	<h3>Click on the fields to fill in your information.</h3>
-	<form class="main" action="index.php?action=createCV" method= "POST">
-	 <h1 id="name"> 
-		<input type="text" id="nameInput" value="Full Name" name="name"></h1>
-	<section class="image" id="image" >
-	
-	<input type="text" id="image"  name="image" value="Insert url for CV photo">
-	</section>
-    <section class="education" id="education">
-        <input type="text" id="bio" rows='7' name="education" value="Enter your education.">
-    </section>
-    
-    <section id="skills">
-        <h2>Skills</h2>
-		<input type='text' id='skill' value="Add Skill" name="skill"/>
-		
-
-                    <ul id='list'>
-
-                    </ul>
-            </section>
-
-    <section id="projects">
-	<h2>Projects</h2>
-	<h3><input id="project" type="text" value="Insert project information" name="project"></h3>
-
-            </section>
 
 
-            <button type="submit">
-                Create CV
-            </button>
-        </form>
-   
+    <h3>Click on the fields to fill in your information.</h3>
+    <form class="main" action="index.php?action=createCV" method="POST">
+        <h1 id="name">
+            <input type="text" id="nameInput" value="Full Name" name="name">
+        </h1>
+        <section class="image" id="image">
+
+            <input type="text" id="image" name="image" value="Insert url for CV photo">
+        </section>
+        <section class="education" id="education">
+            <input type="text" id="bio" name="education" value="Enter your education.">
+        </section>
+
+        <section id="skills">
+            <h2>Skills</h2>
+            <input type='text' id='skill' value="Add Skill" name="skill" />
+            <ul id='list'></ul>
+        </section>
+
+        <section id="skills">
+            <h2>Experience</h2>
+            <input type='text' id='experience' value="Add Experience" name="experience" />
+        </section>n>
+
+        <section id="projects">
+            <h2>Projects</h2>
+            <h3><input id="project" type="text" name="project"value="Insert project information" name="project"></h3>
+
+        </section>
+
+
+        <button type="submit">
+            Create CV
+        </button>
+    </form>
+
 </body>
+
 </html>
